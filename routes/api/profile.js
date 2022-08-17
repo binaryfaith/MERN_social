a1f2b29c9b4e50e866c363d1f4be39e528b4ff67
David Slavich <binaryfaith@gmail.com>
2022-08-17 16:05:18 -0700

    added user login, registration, authentication, token passing, profile model and profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route Get api/profile
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => res.send('Profile route'))
+// @desc Get current users profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
 
-module.exports = router; 
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
